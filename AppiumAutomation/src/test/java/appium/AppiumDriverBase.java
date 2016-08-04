04a5c74ba81f32ceb1d3a1c2282484ea70d6e03f
rajashekarpula <rajashekarpula@live.com>
2016-08-04 20:33:18 +0200

    First

--- AppiumAutomation/src/test/java/appium/AppiumDriverBase.java
+++ AppiumAutomation/src/test/java/appium/AppiumDriverBase.java
@@ -5,7 +5,9 @@ import io.appium.java_client.android.AndroidDriver;
 import java.io.File;
 import java.net.MalformedURLException;
 import java.net.URL;
- 
+
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterTest;
@@ -14,7 +16,7 @@ import org.testng.annotations.BeforeTest;
 public class AppiumDriverBase {
  
     protected AndroidDriver driver;
-    protected WebDriverWait wait;
+    //protected WebDriverWait wait;
  
     //before Test Annotation makes a java function to run every time before a TestNG test case
     @BeforeTest
@@ -27,18 +29,25 @@ public class AppiumDriverBase {
  
     //setting up desired capability
     DesiredCapabilities caps = new DesiredCapabilities();
-    caps.setCapability("browserName", "");
+    /*caps.setCapability("browserName", "");
     caps.setCapability("platform", "ANDROID");
-    caps.setCapability("platformVersion", "6.0");
-    caps.setCapability("deviceName", "ANDROID");
-    caps.setCapability("appPackage", "com.example.raj.myapplication");
-    caps.setCapability("appActivity", "com.example.raj.myapplication.MainActivity");
+    caps.setCapability("platformVersion", "4.2.2");
+    caps.setCapability("deviceName", "ZH8000TNHK");
+    caps.setCapability("appPackage", "org.openintents.shopping");
+    caps.setCapability("appActivity", "org.openintents.shopping.ShoppingActivity");*/
+    caps.setCapability("testobject_api_key", "B16F0B0AEB334DD384FAE69CCFC7C371");
+    caps.setCapability("testobject_app_id", "1");
+    caps.setCapability("testobject_device", "LG_Nexus_4_E960_real");
+    caps.setCapability("testobject_appium_version", "1.5.3");
+    caps.setCapability("testobject_suite_name", "Default Appium Suite");
+    caps.setCapability("testobject_test_name", "Default Appium Test");
  
     //initializing driver object
-    driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+    //driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), caps);
+    driver = new AndroidDriver(new URL("http://appium.testobject.com/wd/hub"), caps);
  
     //initializing explicit wait object
-    wait = new WebDriverWait(driver, 10);
+   // wait = new WebDriverWait(driver, 10);
     }
  
     //After Test Annotation makes a java function to run every time after a TestNG test case
--- AppiumAutomation/src/test/java/appium/SampleTestCase.java
+++ AppiumAutomation/src/test/java/appium/SampleTestCase.java
@@ -4,10 +4,10 @@ import io.appium.java_client.MobileBy;
 
 import java.util.List;
 
-import org.junit.Test;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.testng.annotations.Test;
 
  
 public class SampleTestCase extends AppiumDriverBase{
@@ -15,20 +15,22 @@ public class SampleTestCase extends AppiumDriverBase{
     //Test Annotation changes any java function to TestNG test case
     @Test
     public void sampeTest(){
+    	
+    	driver.findElement(By.className("android.widget.EditText")).sendKeys("Hello");
      
     //click on Accessibility link
-    driver.findElement(By.className("android.widget.Switch")).click();
+    /*driver.findElement(By.className("android.widget.Switch")).click();
     List<WebElement> test = driver.findElements(By.className("android.widget.EditText"));
     test.get(0).sendKeys("Raja Sekhar");
     test.get(1).sendKeys("Ayswarya Narasimhan");
-    driver.findElement(By.className("android.widget.Button")).click();
-    //String result = driver.findElement(By.className("android.widget.TextView")).getText();
-    //System.out.println(result);
-    /*wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Accessibility")));
+    driver.findElement(By.className("android.widget.Button")).click();*/
+    /*String result = driver.findElement(By.className("android.widget.TextView")).getText();
+    System.out.println(result);
+    //wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Accessibility")));
     driver.findElement(MobileBy.AccessibilityId("Accessibility")).click();
      
     //click on 'Accessibility Node Querying' link
-    wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Accessibility Node Querying")));
+    //wait.until(ExpectedConditions.presenceOfElementLocated(MobileBy.AccessibilityId("Accessibility Node Querying")));
     driver.findElement(MobileBy.AccessibilityId("Accessibility Node Querying")).click();
      
     //back
